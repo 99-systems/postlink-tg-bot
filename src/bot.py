4352172dd6b5edf2f643347a7c1f63a26f557f28
Tikava <wavoleniu@gmail.com>
2025-07-12 03:01:00 +0500

    fix: update MongoDB connection string format in bot.py

--- src/bot.py
+++ src/bot.py
@@ -6,5 +6,5 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from src.config import config
 
 bot = Bot(token=config.BOT_TOKEN)
-storage = MongoStorage(client=AsyncIOMotorClient(f"mongodb+srv://{config.MONGO_USER}:{config.MONGO_PASSWORD}@{config.MONGO_HOST}"), db_name=config.MONGO_DB_NAME)
+storage = MongoStorage(client=AsyncIOMotorClient(f"mongodb://{config.MONGO_USER}:{config.MONGO_PASSWORD}@{config.MONGO_HOST}"), db_name=config.MONGO_DB_NAME)
 dp = Dispatcher(storage=storage)
