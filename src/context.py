21dd6a7192a12a433ace92c4965820dd1860887f
Tikava <wavoleniu@gmail.com>
2025-07-31 21:17:20 +0500

    feat: add OTP service configuration and initialize OTPService in context

--- src/context.py
+++ src/context.py
@@ -1,5 +1,5 @@
 from src.services import OTPService, NominatimService
 from src.config import config
 
-# otp_service = OTPService(config.OTP_SERVICE_URL, config.OTP_API_KEY)
+otp_service = OTPService(config.OTP_SERVICE_URL, config.OTP_API_KEY)
 nominatim_service = NominatimService(config.NOMINATIM_API_URL)
