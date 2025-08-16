753db1cb043609aaa736dd0fe9878182318b4430
Shaykhnazar <shayx3470941@gmail.com>
2025-08-16 19:22:33 +0500

    auth remove

--- src/app.py
+++ src/app.py
@@ -1,46 +1,23 @@
 import asyncio
 from src.bot import bot, dp
 
-from src.handlers.request import send_request as send_request_handler, deliver_request as deliver_request_handler, \
-    manage_request as manage_request_handler
-from src.handlers import main as main_handler, auth as auth_handler, support as support_handler, menu as menu_handler, \
-    admin as admin_handler
+from src.handlers import main as main_handler, support as support_handler, admin as admin_handler
 
-from src.middlewares import AuthMiddlewareMessage, LogMiddleware, NotAuthMiddlewareMessage, OpenRequestMessageMiddleware
-from src.middlewares.open_request import OpenRequestCallbackMiddleware
+from src.middlewares import LogMiddleware
 
 
 async def run():
     # Middleware configuration
     main_handler.router.message.middleware.register(LogMiddleware())
-    # main_handler.router.message.middleware.register(AuthMiddlewareMessage())
-    auth_handler.router.message.middleware.register(LogMiddleware())
-    # auth_handler.router.message.middleware.register(NotAuthMiddlewareMessage()) #проверяет на залогиненного пользователя и он больше не зайти в авторизовацию
 
-    menu_handler.router.message.middleware.register(LogMiddleware())
-    menu_handler.router.message.middleware.register(AuthMiddlewareMessage())
-
-    send_request_handler.router.message.middleware(
-        OpenRequestMessageMiddleware())  # проверяет на наличие открытой заявки
-    # send_request_handler.router.callback_query.middleware(OpenRequestCallbackMiddleware()) #проверяет на наличие открытой заявки
-    # deliver_request_handler.router.message.middleware(OpenRequestMessageMiddleware()) #проверяет на наличие открытой заявки
-    # deliver_request_handler.router.callback_query.middleware(OpenRequestCallbackMiddleware()) #проверяет на наличие открытой заявки
 
     # Router configuration
-    menu_handler.router.include_routers(
-        send_request_handler.router,
-        deliver_request_handler.router,
-        manage_request_handler.router,
+    main_handler.router.include_routers(
+        support_handler.router,
         admin_handler.router
     )
 
-    main_handler.router.include_routers(
-        support_handler.router,
-        menu_handler.router
-    )
-
     dp.include_routers(
-        auth_handler.router,
         main_handler.router,
     )
 
--- src/handlers/main.py
+++ src/handlers/main.py
@@ -1,36 +1,22 @@
 from aiogram.fsm.context import FSMContext
 from aiogram import Router, F
-from aiogram.filters import Command, StateFilter
+from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardRemove
 
-import src.database.models.crud as crud
-from src.common.states import AppState, SupportState, RegistrationState, LoginState
+from src.common.states import SupportState
 from src.common import keyboard as kb
-from src.database import db
 
 router = Router()
 
 
-@router.message(StateFilter(None, AppState.auth), Command("start"))
+@router.message(Command("start"))
 async def start(message: Message, state: FSMContext):
-    await state.update_data(tg_user_id=message.from_user.id)
-
-    tg_user = crud.get_tg_user(db, message.from_user.id)
-
-    if not tg_user:
-        tg_user = crud.add_tg_user(db, message.from_user.id, message.from_user.username)
-
-    await handle_start(message, state)
+    await message.answer('Приступим к работе!')
 
 
-@router.message(RegistrationState.name, F.text.lower() == 'назад')
-@router.message(LoginState.phone, F.text.lower() == 'назад')
-async def handle_start(message: Message, state: FSMContext):
-    await message.answer('Приступим к работе!', reply_markup=kb.auth_reply_mu)
-    await state.set_state(AppState.auth)
 
 
-@router.message(F.text.lower() == 'служба поддержки', AppState.menu)
+@router.message(F.text.lower() == 'служба поддержки')
 async def handle_support(message: Message, state: FSMContext):
     await message.answer(
         'Привет! Это служба поддержки PostLink.\nПожалуйста, ответьте на несколько вопросов, и мы сделаем все, что в наших силах!',
