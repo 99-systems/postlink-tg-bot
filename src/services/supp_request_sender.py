11b6fb17fefa72ee9eb7744c4d3f57f34925de03
Shaykhnazar <shayx3470941@gmail.com>
2025-09-16 23:32:28 +0500

    fix sending support request issue 3

--- src/services/supp_request_sender.py
+++ src/services/supp_request_sender.py
@@ -19,39 +19,42 @@ async def send_supp_request(supp_req_id: int):
             return
 
         user = None
+        user_name = None
+        user_phone = None
+        user_tg_username = None
+        user_tg_id = None
+        tg_username = None
+        tg_id = None
+        message = supp_req.message
+
         if supp_req.user:
             user = crud.get_user_by_tg_id(db, supp_req.user.telegram_user.telegram)
-    
-    if user:
-        # reqs = crud.get_all_req_ids_by_user(db, user)
-        text = f'Поступила новая заявка на поддержку от пользователя {user.name} ({user.phone}).'
+            if user:
+                user_name = user.name
+                user_phone = user.phone
+                if user.telegram_user:
+                    user_tg_username = user.telegram_user.username
+                    user_tg_id = user.telegram_user.telegram
+
+        if supp_req.telegram_user:
+            tg_username = supp_req.telegram_user.username
+            tg_id = supp_req.telegram_user.telegram
+
+    if user_name and user_phone:
+        text = f'Поступила новая заявка на поддержку от пользователя {user_name} ({user_phone}).'
     else:
-        tg_user = supp_req.telegram_user
-        if tg_user:
-            username = f"@{tg_user.username}" if hasattr(tg_user, 'username') and tg_user.username else "без имени пользователя"
-            text = f'Поступила новая заявка на поддержку от пользователя с ID {tg_user.telegram} и {username}.'
+        if tg_id:
+            username_text = f"@{tg_username}" if tg_username else "без имени пользователя"
+            text = f'Поступила новая заявка на поддержку от пользователя с ID {tg_id} и {username_text}.'
         else:
             text = 'Поступила новая заявка на поддержку от неизвестного пользователя.'
 
-    text += f'\nТекст заявки: {supp_req.message}'
+    text += f'\nТекст заявки: {message}'
 
-
-
-    if user:
-        tg_user = user.telegram_user
-        if hasattr(tg_user, 'username') and tg_user.username:
-            text += f'\n\n<b>Информация о пользователе:</b>'
-            text += f'\nИмя пользователя: @{tg_user.username}'
-            text += f'\nID пользователя: {tg_user.telegram}'
-        
-        # text += f'\n\n<b>Все заявки пользователя:</b>'
-        # send_ids = reqs['send']
-        # text += f'\nНа отправку: '
-        # text += ', '.join([str(i) for i in send_ids]) if send_ids else 'Отсутствуют'
-        #
-        # delivery_ids = reqs['delivery']
-        # text += f'\nНа доставку: '
-        # text += ', '.join([str(i) for i in delivery_ids]) if delivery_ids else 'Отсутствуют'
+    if user_name and user_tg_username and user_tg_id:
+        text += f'\n\n<b>Информация о пользователе:</b>'
+        text += f'\nИмя пользователя: @{user_tg_username}'
+        text += f'\nID пользователя: {user_tg_id}'
 
     await bot.send_message(chat_id, text, parse_mode='HTML')
 
@@ -66,9 +69,11 @@ async def send_message_to_admins(message: str):
 async def send_message_to_all_users(message: str):
     with get_db() as db:
         users = crud.get_all_users(db)
-    for user in users:
+        user_telegrams = [user.telegram_user.telegram for user in users if user.telegram_user]
+
+    for telegram_id in user_telegrams:
         try:
-            await bot.send_message(user.telegram_user.telegram, message)
+            await bot.send_message(telegram_id, message)
             await asyncio.sleep(0.1)
         except Exception as e:
             print(e)
@@ -101,9 +106,16 @@ async def get_supp_request_from_message(message: str):
 async def close_supp_request(message: str):
     supp_req = await get_supp_request_from_message(message)
     if supp_req:
+        telegram_id = None
         with get_db() as db:
-            crud.close_supp_request(db, supp_req)
-        await bot.send_message(supp_req.telegram_user.telegram, 'Ваша заявка в поддержку была закрыта.')
+            # Get fresh support request with relationships
+            fresh_supp_req = crud.get_supp_request_by_id(db, supp_req.id)
+            if fresh_supp_req and fresh_supp_req.telegram_user:
+                telegram_id = fresh_supp_req.telegram_user.telegram
+                crud.close_supp_request(db, fresh_supp_req)
+
+        if telegram_id:
+            await bot.send_message(telegram_id, 'Ваша заявка в поддержку была закрыта.')
         return True
     return False
     
